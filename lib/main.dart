b251b30d40e6fb153911e968f31f9d3e91b35aab
khofifah18 <khofifahainunrohma@gmail.com>
2022-11-13 20:59:31 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'controller/DemoController.dart';
+import 'view/DemoPage.dart';
+import 'view/home.dart';
+
+void main() async {
+  await GetStorage.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final DemoController ctrl = Get.put(DemoController());
+  @override
+  Widget build(BuildContext context) {
+    return SimpleBuilder(builder: (_) {
+      return GetMaterialApp(
+        title: 'GetX',
+        theme: ctrl.theme,
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomePage(),
+          '/cart': (context) => DemoPage(),
+        },
+      );
+    });
+  }
+}
---
+++ lib/view/DemoPage.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/DemoController.dart';
+
+class DemoPage extends StatelessWidget {
+  final DemoController ctrl = Get.find();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Demo Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(Get.arguments),
+            ),
+            SwitchListTile(
+              value: ctrl.isDark,
+              title: Text("Touch to change TheMode"),
+              onChanged: ctrl.changeTheme,
+            ),
+            ElevatedButton(
+                onPressed: () => Get.snackbar(
+                    "Snackbar", "Hello this is the Snackbar message",
+                    snackPosition: SnackPosition.BOTTOM,
+                    colorText: Colors.white,
+                    backgroundColor: Colors.black87),
+                child: Text('Snack Bar')),
+            ElevatedButton(
+                onPressed: () => Get.defaultDialog(
+                    title: "Dialogue",
+                    content: Text(
+                      'Hey, From Dialogue',
+                    )),
+                child: Text('Dialogue')),
+            ElevatedButton(
+                onPressed: () => Get.bottomSheet(Container(
+                      height: 150,
+                      color: Colors.white,
+                      child: Text(
+                        'Hello, From BottomSheet',
+                        style: TextStyle(fontSize: 30.0),
+                      ),
+                    )),
+                child: Text('BottomSheet')),
+            ElevatedButton(
+                onPressed: () => Get.offNamed('/'),
+                child: Text('Back To Home')),
+          ],
+        ),
+      ),
+    );
+  }
+}
